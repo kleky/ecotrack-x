70271b793e2bb0ff154d74393ed90aa422393b06
ian klek <ian.klek@gmail.com>
2019-09-17 20:20:12 +0100

    tab and geolocation properly should be working

---
+++ readme.md
@@ -0,0 +1,9 @@
+#### Build
+`ionic build`
+
+#### Sync to android studio
+`npx cap sync`
+
+#### Open studio
+`npx cap open android`
+
--- src/app/speed-timer/clock/clock.component.ts
+++ src/app/speed-timer/clock/clock.component.ts
@@ -1,32 +1,39 @@
-import { Component, OnInit } from "@angular/core";
-import {Geolocation, Geoposition} from "@ionic-native/geolocation/ngx";
-import {filter, tap} from "rxjs/operators";
+import {Component, OnInit} from "@angular/core";
 import {Logger} from "ionic-logging-service";
 import {LogService} from "../../logger/services/log.service";
+import {Geolocation, GeolocationPosition} from "@capacitor/core";
+
 
 @Component({
-  selector: "app-clock",
-  templateUrl: "./clock.component.html",
-  styleUrls: ["./clock.component.scss"],
+    selector: "app-clock",
+    templateUrl: "./clock.component.html",
+    styleUrls: ["./clock.component.scss"],
 })
 export class ClockComponent implements OnInit {
 
-  private logger: Logger;
-  speed: number;
+    private logger: Logger;
+    speed: number;
 
-  constructor(private geolocation: Geolocation,
-              private logService: LogService) {
-    this.logger = this.logService.getLogger(`Clock`);
-  }
+    constructor(private logService: LogService) {
+        this.logger = this.logService.getLogger(`Clock`);
+    }
 
-  ngOnInit() {
-    const watch = this.geolocation.watchPosition();
-    watch.pipe(
-        tap(e => this.logger.debug("Init", "Watch position result", e)),
-        filter((p) => p.coords !== undefined)
-    ).subscribe((data) => {
-        this.speed = data.coords.speed;
-    });
-  }
+    ngOnInit() {
+        Geolocation.watchPosition({
+            enableHighAccuracy: true,
+            timeout: 6000,
+            maximumAge: 6000,
+            requireAltitude: false,
+        }, this.updatePosition);
+    }
+
+    updatePosition = (position: GeolocationPosition, err?: any) => {
+        if (err) {
+            console.error("Error updating position", err);
+        } else {
+            console.log("Updating position", position);
+            this.speed = data.coords.speed;
+        }
+    }
 
 }
